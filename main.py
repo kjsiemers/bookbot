fe7f5b6257df2ffc2a36bef278a226b72658a14d
kjsiemers <kjs@umich.edu>
2026-01-01 16:20:22 -0500

    add letter counts

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-from stats import get_num_words
+from stats import get_num_words, get_letter_counts
 
-get_num_words()
+path_to_file = "/home/kent/projects/github.com/bookbot/books/frankenstein.txt"
+num_words = get_num_words(path_to_file)
+print(f"Found {num_words} total words")
+counts = get_letter_counts(path_to_file)
+for k in counts:
+    print(f"{k}: {counts[k]}")
--- stats.py
+++ stats.py
@@ -1,4 +1,4 @@
-def get_book_text(path_to_file):
+def get_book_text(path_to_file):   
     with open(path_to_file) as f:
         # do something with f (the file) here
 
@@ -6,9 +6,25 @@ def get_book_text(path_to_file):
         file_contents = f.read()
         return file_contents
 
-def get_num_words():
-    path_to_file = "/home/kent/projects/github.com/bookbot/books/frankenstein.txt"
+def get_num_words(path_to_file):
     t = get_book_text(path_to_file)
     list_of_words = t.split()
     num_words = len(list_of_words)
-    print(f"Found {num_words} total words")
+    return num_words
+
+def get_letter_counts(path_to_file):
+    counts ={}
+    n = 0
+    t = get_book_text(path_to_file)
+    for i in range(0, len(t)):
+        mixed = t[i:i+1:2]
+        letter = mixed.lower()
+        if letter in counts:
+            n = counts[letter]
+            n +=1
+            counts[letter] = n
+        else:
+            # we found a new letter
+            counts[letter] = 1
+    return counts
+
